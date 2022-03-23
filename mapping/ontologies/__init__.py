a7966be5ae334eccb785b68a7e1633cc56c8bbaf
Majid alDosari <majid.aldosari@pnnl.gov>
2022-03-23 18:13:07 -0400

    [mapping] use filenames to id ontologies

--- mapping/ontologies/__init__.py
+++ mapping/ontologies/__init__.py
@@ -1,4 +1,29 @@
-def get():
-    from .get import get_223p, get_brick
-    get_brick()
-    get_223p()
+
+
+
+def groups():
+    from pathlib import Path
+    dir = Path(__file__).parent
+    for i in dir.iterdir():
+        if i.is_file():
+            if i.suffix == '.ttl':
+                yield i.stem.lower(), i
+        else:
+            for ii in i.iterdir():
+                if ii.suffix == '.ttl':
+                    yield i.stem.lower(), ii
+
+names = frozenset(n for n, _ in groups())
+
+
+def get(name: str):
+    assert(name in names)
+    import rdflib
+    g = rdflib.Graph()
+    for n, f in groups(): g += rdflib.Graph().parse(f)
+    return g
+
+
+def init():
+    from .get import dl_brick
+    dl_brick()
--- mapping/ontologies/get.py
+++ mapping/ontologies/get.py
@@ -3,14 +3,7 @@ from pathlib import Path
 onto_loc = Path(__file__).parent
 
 
-def get_223p() -> rdflib.Graph:
-    g = rdflib.Graph()
-    for f in (f for f in ( onto_loc / 'reference-223p').iterdir() if f.suffix == '.ttl'):
-        g += rdflib.Graph().parse(f)
-    return g
-
-
-def get_brick() -> rdflib.Graph:
+def dl_brick() -> rdflib.Graph:
     dst = onto_loc / 'Brick.ttl'
     src = 'https://brickschema.org/schema/Brick'
     if not dst.exists():
