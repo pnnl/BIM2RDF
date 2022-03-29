25975f378f79ec7f94e453dfb6fe6518ce83b4b6
Majid alDosari <majid.aldosari@pnnl.gov>
2022-03-29 14:53:56 -0400

    [mapping] further abstracting classes

--- mapping/mapping/schema.py
+++ mapping/mapping/schema.py
@@ -1,37 +1,86 @@
 from attrs import frozen as dataclass
 from rdflib import Graph
-from collections.abc import Mapping
-from typing import Any
-
-@dataclass
-class DBProperties:
-    name: str
-    url: str
-    driver: str
-    user: str
-
-@dataclass
-class OntopProperties:
-    inferDefaultDatatype: bool
-
-@dataclass  
-class Properties:
-    jdbc:   DBProperties
-    ontop:  OntopProperties
+from collections.abc import Mapping 
+from typing import Any, Callable, TypeVar, Generic
+from abc import ABC, abstractmethod, abstractclassmethod
 
 
-@dataclass
-class Ontology:
-    name: str
-    graph: Graph
+def abstractfield(f: Callable):
+    _ = abstractmethod(f)
+    _ = property(_)
+    return _
 
-@dataclass
-class OntologyCustomization:
-    building: str
-    ontology: Ontology
 
-@dataclass
-class SQLRDFMap:
-    ontology: Graph # not necessarily the ontology above
-    mapping: Mapping[str, Any]
-    properties: Properties
+
+T = TypeVar('T')
+class Construtor(Generic[T], ABC): 
+    @abstractclassmethod
+    def make(cls, *p, **k) -> T: ...
+
+
+@dataclass # looks like i have to put this here
+class Base(Construtor): pass
+
+class DBProperties(Base):
+    @abstractfield
+    def name(self)          -> str: ...
+    @abstractfield
+    def url(self)           -> str: ...
+    @abstractfield
+    def driver(self)        -> str: ...
+    @abstractfield
+    def user(self)          -> str: ...
+
+
+#@dataclass
+#class DBPropertiesImpl(DBProperties):
+    #notname: str #cant instantiate
+    #name: int # can but is it typechecked?
+#DBPropertiesImpl('sdf')
+
+
+class OntopProperties(Base):
+    @abstractfield
+    def inferDefaultDatatype(self) \
+                            -> bool: ...
+
+
+class Properties(Base):
+    @abstractfield
+    def jdbc(self)          -> DBProperties: ...
+    @abstractfield
+    def ontop(self)         -> OntopProperties: ...
+
+
+class OntologyBase(Base):
+    @abstractfield
+    def name(self)          -> str: ...
+    @abstractfield
+    def graph(self)         -> Graph: ...
+
+
+class OntologyCustomization(Base):
+    @abstractfield
+    def building(self)      ->  str: ...
+    @abstractfield
+    def base(self)          ->  OntologyBase: ...
+
+
+class Ontology(Base):
+    @abstractfield
+    def base(self)          -> OntologyBase: ...
+    @abstractfield
+    def customization(self) -> OntologyCustomization: ...
+
+    @abstractmethod
+    def graph(self)         -> Graph: ...
+
+
+class SQLRDFMap(Base):
+    @abstractfield
+    def ontology(self)      -> Ontology: ...
+    @abstractfield
+    def mapping(self)       -> Mapping[str, Any]: ... # todo typeddict for further
+    @abstractfield
+    def properties(self)    -> Properties: ...
+
