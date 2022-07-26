3195e2ebd9f62e5857c330cf5cbeb6f31805f4bd
Majid alDosari <majid.aldosari@pnnl.gov>
2022-07-26 12:21:56 -0400

    [mapping] if bdg not given, take from db name

--- mapping/tasks.py
+++ mapping/tasks.py
@@ -7,15 +7,18 @@ def get_ontologies(ctx):
     init()
 
 
-
-
 from pathlib import Path
 from project import activated_workdir
 
+
 @task
-def map(ctx, bdg, ontology, db_file=str(activated_workdir / 'work' /'db.sqlite')):
+def map(ctx, ontology, bdg=None,  db_file=str(activated_workdir / 'work' /'db.sqlite')):
+    # db
     db_file = Path(db_file)
     if not db_file.exists(): raise ValueError('db file doesnt exist')
+    # bdg
+    if not bdg: bdg = db_file.stem
+
     import mapping.mapping as m
     sr = m.SQLRDFMapping.make((bdg, ontology, m.SQLiteDB( db_file) , ))
     sr.map(db_file.parent)
