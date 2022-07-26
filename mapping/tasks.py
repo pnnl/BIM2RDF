01b247fa7eeeb28b4aa3a1b2bb06d8657a27b4e5
Majid alDosari <majid.aldosari@pnnl.gov>
2022-07-26 10:58:52 -0400

    [mapping] fix: take db path from arg

--- mapping/tasks.py
+++ mapping/tasks.py
@@ -14,7 +14,7 @@ from project import activated_workdir
 
 @task
 def map(ctx, bdg, ontology, db_file=str(activated_workdir / 'work' /'db.sqlite')):
-    db_file = Path(activated_workdir / 'work' /'db.sqlite')
+    db_file = Path(db_file)
     if not db_file.exists(): raise ValueError('db file doesnt exist')
     import mapping.mapping as m
     sr = m.SQLRDFMapping.make((bdg, ontology, m.SQLiteDB( db_file) , ))
