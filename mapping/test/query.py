b0da9537871e84bfb42b79785d927d96014b8412
Majid alDosari <majid.aldosari@pnnl.gov>
2022-11-22 12:32:04 -0500

    [mapping] construct lpd sparql

--- mapping/test/query.py
+++ mapping/test/query.py
@@ -2,6 +2,7 @@ q = """
 PREFIX brick: <https://brickschema.org/schema/Brick#>
 PREFIX xsd:   <http://www.w3.org/2001/XMLSchema#>
 
+
 # 'overall' check for shacl
 select (?checks = ?count  as ?this) {
 
@@ -24,6 +25,49 @@ select   ( sum(if(?loc_lpd < 100, 1, 0)) as ?checks)  (count(?loc_lpd) as ?count
   }
 
 }
+
+"""
+q = """
+PREFIX brick: <https://brickschema.org/schema/Brick#>
+PREFIX xsd:   <http://www.w3.org/2001/XMLSchema#>
+
+construct {?loc brick:value ?loc_lpd} {   
+
+# 'check' level
+#        using if b/c apparently ops on boolean dont see it as numeric
+select   ?loc  ?loc_lpd    {
+
+# 'lower-level': agg. by room
+select ?loc_type  ?loc ?area  (sum(?power/?area) as ?loc_lpd)  (sum(?power) as ?loc_power )     { 
+?luminaire a brick:Luminaire. 
+?luminaire brick:hasLocation ?loc. 
+?luminaire brick:hasLocation/a ?loc_type. 
+?loc_type rdfs:subClassOf brick:Room. # in owl is room a subclass of itself?? 
+#?loc_type sesame:directSubClassOf brick:Room. # elimnates 'synonyms' and going up the heirarchy
+?luminaire brick:ratedPowerInput/brick:value ?power.
+?loc brick:grossArea/brick:value ?area. 
+#?loc a brick:Mechanical_Room. # testing filter
+} group by ?loc_type ?loc ?area
+
+}
+}
+"""
+q = """
+    PREFIX brick: <https://brickschema.org/schema/Brick#>
+    PREFIX xsd:   <http://www.w3.org/2001/XMLSchema#>
+    
+    
+    construct {?this brick:value ?loc_lpd } {
+    
+    select ?this ?area  (sum(?power/?area) as ?loc_lpd)  (sum(?power) as ?loc_power )  { 
+    ?luminaire a brick:Luminaire.
+    ?luminaire brick:hasLocation ?this.
+    ?luminaire brick:ratedPowerInput/brick:value ?power.
+    ?this brick:grossArea/brick:value ?area.
+    } group by ?this
+
+    }
+
 
 """
 
