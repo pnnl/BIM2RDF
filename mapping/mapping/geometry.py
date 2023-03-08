5142fd9be2b0832dbaef9d1b6f83703ba5afa45a
Majid alDosari <majid.aldosari@pnnl.gov>
2023-03-08 12:22:36 -0500

    [mapping] geometry  test

--- mapping/mapping/geometry.py
+++ mapping/mapping/geometry.py
@@ -1,13 +1,13 @@
 from typing import Tuple, Iterable
 
 Point = Tuple[float, float, float]
-def point_inside_points(
+def is_point_inside_points(
         pt:             Point,
         pts: Iterable[  Point ]) \
     -> bool:
     """inside ...or touching"""
     if len(pts) < 2:
-        raise ValueError('need at least to pts to make sense')
+        raise ValueError('need at least two pts to make sense')
     mnx = min(p[0] for p in pts)
     mny = min(p[1] for p in pts)
     mnz = min(p[2] for p in pts)
@@ -23,3 +23,47 @@ def point_inside_points(
     else:
         return False
 
+
+def list_to_pts(l):
+    l = list(l)
+    assert(not len(l) % 3)
+    # take every 3rd
+    xs = (l)[0::3]
+    ys = (l)[1::3]
+    zs = (l)[2::3]
+    _ = zip(xs, ys, zs)
+    _ = list(_)
+    return _
+
+
+def test():
+    def qp(r): print(*r,sep='\n')
+
+    import speckle.graphql as sg
+    import speckle.objects as o
+    d=sg.query(sg.queries().objects)
+    g=o.rdf(d)
+    qr = g.query("""
+    PREFIX spkl: <http://speckle.systems/>
+    select ?m  ?v
+        where {
+            ?m spkl:speckle_type "Objects.Geometry.Mesh".
+            ?m spkl:vertices/spkl:data ?v. 
+            spkl:cd987c37645ab9e185590d290c3d5223 spkl:speckle_type "Speckle.Core.Models.DataChunk".
+        }
+    """)
+    _=qr
+    qp(qr)
+    return _
+    qr=g.query("""
+      PREFIX spkl: <http://speckle.systems/>
+      select ?p ?x ?y ?z
+        where {
+        ?p spkl:category "Lighting Fixtures". # filter
+        ?p spkl:basePoint/spkl:x ?x.
+        ?p spkl:basePoint/spkl:y ?y.
+        ?p spkl:basePoint/spkl:z ?z.
+        }
+    """)
+    _ = qr
+    return _
