6c2a82c0aba00f84f961f714e4d961cd2f04e438
Majid alDosari <majid.aldosari@pnnl.gov>
2023-03-09 14:13:44 -0500

    [mapping] db loading data and onto

---
+++ mapping/mapping/engine.py
@@ -0,0 +1,9 @@
+"""
+engine specialization
+"""
+from engine.triples import (
+        ConstructQuery,
+        Rules,
+        Rule ,  # for owlrl TODO
+        Engine, OxiGraph)
+
--- mapping/mapping/speckle.py
+++ mapping/mapping/speckle.py
@@ -1,32 +1,63 @@
 
-from speckle.graphql import queries, query
-from speckle.objects import rdf
-from shacl_gen import shacl
-from ontologies import get as get_ontology
-from . import mapping_dir
+
+from .engine import Rules
+def get_rules() -> Rules: #sparql mapping 'rules' for now 
+    from .engine import ConstructQuery, Rules
+    from . import mapping_dir
+    from pathlib import Path
+    _ = Path(mapping_dir).glob('**/*.sparql')
+    _ = map(lambda p: open(p).read(),   _)
+    _ = map(ConstructQuery,             _)
+    _ = Rules(_)
+    return _
+
+
+# try:
+#     from typing import Self
+#     print('upgrade to python 3.11')
+# except:
+#     from typing_extensions import Self
+
+# from .engine import Rules as _Rules
+# class Rules(_Rules):
+#     # def __new__(cls) -> Self:
+#     #     return super().__new__()
+#     @classmethod
+#     def __call__(cls) -> '_Rules':
+#         return get_rules()
+
 
 # (query>json) > ttl > mapping/validation via shacl > ttl
 
-from pathlib import Path
-def brick() -> Path:
-    _ = mapping_dir / 'speckle' / 'brick.ttl'
-    assert(_.exists())
-    return _
+
+def map_():
+    from speckle.graphql import queries, query
+    _ = queries()
+    _ = _.objects
+    _ = query(_) # dict
+    from speckle.objects import rdf
+    _ = rdf(_)
+    _  = _.encode() # bytes
+    fmt = 'application/n-quads'
+    #_ = parse(_, fmt) 
+    from pyoxigraph import Store
+    db = Store()
+    db.bulk_load(_, fmt)
+    r = get_rules()     # mapping
+    from ontologies import get as get_ontology
+    _ = get_ontology('brick')
+    _ = open(_, 'rb')
+    db.bulk_load(_, 'text/turtle')
+    #from shacl_gen import shacl
+    return db
 
 
-def map():
-    q = queries()
-    q = q.objects
-    d = query(q)    # json
-    d = rdf(d)      # rdf
-    m = brick()     # mapping
-    o = get_ontology('brick')
-    _ = shacl(d, shacl=m, ontology=o)
+def test():
+    _ = map_()
     return _
-    # gen data (rule effects)
 
 
 if __name__ == '__main__':
-    #from speckle.objects import rdf
-    map()
+    ...
+
 
