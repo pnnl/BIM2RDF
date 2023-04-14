6539eb2fc6a992046cc0435d2c17ee94ab87ced6
Majid alDosari <majid.aldosari@pnnl.gov>
2023-04-14 15:49:51 -0400

    [mapping] conversions

--- mapping/mapping/conversions.py
+++ mapping/mapping/conversions.py
@@ -1,5 +1,7 @@
 from rdflib import Graph
 from pyoxigraph import Store
+
+
 def og2rg(og: Store,) -> Graph:
     from io import BytesIO
     _ = BytesIO()
@@ -8,3 +10,16 @@ def og2rg(og: Store,) -> Graph:
     _.seek(0)
     _ = Graph().parse(_, to)
     return _
+
+
+from io import BytesIO
+def rg2og(r: Graph) -> Store:
+    to = 'text/turtle'
+    _ = BytesIO()
+    r.serialize(_, to)
+    from pyoxigraph import Store
+    _.seek(0)
+    r: Store = Store()
+    r.load(_, to) # where illegal rdf with literal subjects is an issue
+    return r
+
