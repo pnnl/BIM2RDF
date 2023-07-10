80298d14009f333be55b38874598198be2490bd6
Bilal Faye <bilal.faye@pnnl.gov>
2023-07-10 11:35:42 -0700

    [speckle] fix: adding task in config

--- speckle/src/speckle/config.py
+++ speckle/src/speckle/config.py
@@ -1,23 +1,30 @@
+
 def config():
-    #from speckle.requests import TokenAuth #get_cached_session TokenAuth
-    #TokenAuth()
-    from speckle.server import (
-        apikey, test, contextualize, rdf
-    )
+    from speckle.server import apikey, test, contextualize, rdf
     contextualize()
     rdf()
     test()
     apikey()
-
-    from .requests import TokenAuth, get_cached_session
+    from speckle.requests import TokenAuth, get_cached_session
     TokenAuth()
     get_cached_session()
-    from .meta import 
-
-   
-
-    #get_cached_session()
+    from speckle.meta import contextualize, rdf
+    contextualize()
+    rdf()
+    from speckle.objects import find_list_fields, sample_json, remove_at,url_quote,rdf
+    find_list_fields()
+    sample_json()
+    remove_at()
+    url_quote()
+    rdf()
+    from speckle.graphql import CachedRequestHTTPTransport, client, get_schema, get_dsl_schema, get_void_query,query,queries
+    CachedRequestHTTPTransport()
+    client()
+    get_schema()
+    get_dsl_schema()
+    get_cached_session()
+    query()
+    queries() 
     
-
 if __name__ == '__main__':
     config()
