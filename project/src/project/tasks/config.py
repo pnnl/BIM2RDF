f602821f08b491ca3a6d2ec677ba10b2a5ef526d
Bilal Faye <bilal.faye@pnnl.gov>
2023-07-10 11:44:54 -0700

    [project] triger speckle config

--- project/src/project/tasks/config.py
+++ project/src/project/tasks/config.py
@@ -4,6 +4,7 @@
 def config(dev = True):
     from .git import config as gitconfig
     if dev: gitconfig()
+    from speckle.config import config; config()
     # TODO: trigger configs from each workspace.
     # from <workspace>.config import config; config()
     # ...etc.
