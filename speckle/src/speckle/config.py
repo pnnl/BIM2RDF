6a0b27c1b2ea6595fb7fcae7c591275f60aedb60
Bilal Faye <bilal.faye@pnnl.gov>
2023-07-12 12:27:05 -0700

    [speckle] fix: correcting config after discussion

--- speckle/src/speckle/config.py
+++ speckle/src/speckle/config.py
@@ -1,30 +1,16 @@
 
 def config():
-    from speckle.server import apikey, test, contextualize, rdf
-    contextualize()
-    rdf()
-    test()
-    apikey()
-    from speckle.requests import TokenAuth, get_cached_session
-    TokenAuth()
-    get_cached_session()
-    from speckle.meta import contextualize, rdf
-    contextualize()
-    rdf()
-    from speckle.objects import find_list_fields, sample_json, remove_at,url_quote,rdf
-    find_list_fields()
-    sample_json()
-    remove_at()
-    url_quote()
-    rdf()
-    from speckle.graphql import CachedRequestHTTPTransport, client, get_schema, get_dsl_schema, get_void_query,query,queries
-    CachedRequestHTTPTransport()
-    client()
-    get_schema()
-    get_dsl_schema()
-    get_cached_session()
-    query()
-    queries() 
+    import os.path 
+    from project import root
+    _= root / 'speckle' / 'secret.json'
+    if os.path.isfile(_) == True: 
+        import json
+        input_data = input("enter your token: ")
+        json_object = {"speckl_token": input_data}
+        with open("secret.json","w") as f:
+            json.dump(json_object,f)
+    else: 
+        return None
     
 if __name__ == '__main__':
     config()
