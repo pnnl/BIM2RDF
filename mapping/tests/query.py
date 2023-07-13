552219b489cd6c7a747e8e4f51883a0a64f387f3
Majid alDosari <majid.aldosari@pnnl.gov>
2023-07-13 17:47:16 -0400

    [mapping][speckle] accounting speckle branch in geometry calc

--- mapping/tests/query.py
+++ mapping/tests/query.py
@@ -33,17 +33,33 @@ order by ?vl ?pos
 limit 30
 """
 
-# q = """\
-# PREFIX spkl: <http://speckle.systems/>
-# PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-# PREFIX meta: <http://meta>
+q = """\
+PREFIX spkl: <http://speckle.systems/>
+PREFIX meta: <http://meta>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-# select ?s where {
-# # <<<http://speckle.systems/4b6532afca46ed30376342826365a14e> <http://speckle.systems/systemName> "Lighting Fixtures">> <http://meta> <<<http://speckle.systems/8e8a907d59> <http://speckle.systems/name> "electrical">> .
-# <<?s spkl:systemName "Lighting Fixtures" >> meta:  <<?s spkl:name "electrical">> .
+select  ?s ?vl  (count(?f)-1 as ?pos) ?xyz
+where {
 
-# }
-# """
+?s spkl:category "Lighting Fixtures".
+?s spkl:transform/spkl:matrix ?vl.
+    # path 'parts' must contain: dispalyValue, vertices, and data
+    # connect them in whatever way
+    #?m spkl:speckle_type "Objects.Geometry.Mesh".
+    #?dc spkl:speckle_type "Speckle.Core.Models.DataChunk".
+    #?m  spkl:vertices ?vl.
+    #?vl rdf:rest*/rdf:first ?xyz.  # order not guaranteed!
+    ?vl rdf:rest* ?f. ?f rdf:rest* ?n. # conects (first, next) ptrs to data list
+    ?n rdf:first ?xyz.
+
+# tricky!
+<<?n rdf:first ?xyz >> meta: <<?branch spkl:name "electrical" >>.
+<<?s spkl:category "Lighting Fixtures" >> meta: <<?branch spkl:name "electrical" >>.
+
+}
+group by ?s ?vl ?n ?xyz
+order by ?vl ?pos
+"""
 
 
 def query(q, ttl):
