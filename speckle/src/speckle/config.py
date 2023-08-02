8e2a643a61c5534d00e98d2b3e66e747fc4c7722
Bilal Faye <bilal.faye@pnnl.gov>
2023-08-02 13:04:35 -0700

    [speckle] fix: use pathlib and correcting open()

--- speckle/src/speckle/config.py
+++ speckle/src/speckle/config.py
@@ -1,14 +1,14 @@
 
-def config():
-    import os.path 
-    from project import root
-    _= root / 'speckle' / 'secret.json'
-    bdir = root / 'speckle'
-    if os.path.isfile(_) == False: 
+def config(dev = True):
+    import pathlib
+    file_loc = pathlib.Path.cwd().parent.parent
+    file_name = 'secretz.json'
+    _ = file_loc / file_name
+    if _.exists() == False: 
         import json
         input_data = input("enter your token: ")
         json_object = {"speckl_token": input_data}
-        with open(os.path.join(bdir,"secret.json"),"w") as f:
+        with open(_,"w") as f:
             json.dump(json_object,f)
     else: 
         return None
