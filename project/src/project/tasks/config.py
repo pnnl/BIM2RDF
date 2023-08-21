a8205fdd550dfa3a268d9ddf643694aa5d64b3e8
Bilal Faye <bilal.faye@pnnl.gov>
2023-08-21 15:29:01 -0700

    [project] taking out to focus on mapping runs

--- project/src/project/tasks/config.py
+++ project/src/project/tasks/config.py
@@ -4,10 +4,7 @@
 def config(dev = True):
     from .git import config as gitconfig
     if dev: gitconfig()
-    from speckle.config import config; config()
-    from project_azure.config import config; config()
-    from project_dvc.config import config; config()
-    from graphdb.graphdb import repo_config; repo_config()
+    #from speckle.config import config; config()
     # TODO: trigger configs from each workspace.
     # from <workspace>.config import config; config()
     # ...etc.
