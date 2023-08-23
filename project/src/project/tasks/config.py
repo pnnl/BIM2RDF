2d8eba0b2eeaca18f91229bbe515368d966745b2
Bilal Faye <bilal.faye@pnnl.gov>
2023-08-23 09:21:50 -0700

    [project] fix: passing dev arg

--- project/src/project/tasks/config.py
+++ project/src/project/tasks/config.py
@@ -4,7 +4,9 @@
 def config(dev = True):
     from .git import config as gitconfig
     if dev: gitconfig()
-    from speckle.config import config; config(dev=True)
+    from speckle.config import config; config(dev=dev)
+    from project_azure.config import config; config(dev=dev)
+    from graphdb.graphdb import config; config(dev=dev)
     # TODO: trigger configs from each workspace.
     # from <workspace>.config import config; config()
     # ...etc.
