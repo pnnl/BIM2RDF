093cf5c0584663c3fb1899eaa20ba7bb1a20b37d
Bilal Faye <bilal.faye@pnnl.gov>
2023-08-24 09:43:21 -0700

    [project] fix submodule ref to track git.py

--- project/src/project/tasks/config.py
+++ project/src/project/tasks/config.py
@@ -2,11 +2,11 @@
 
 
 def config(dev = True):
-    from .git import config as gitconfig
-    if dev: gitconfig()
+    from project.tasks.git import config as gitconfig
+    if dev: gitconfig(dev=dev)
     from speckle.config import config; config(dev=dev)
     from project_azure.config import config; config(dev=dev)
-    from graphdb.graphdb import config; config(dev=dev)
+    #from graphdb.graphdb import config; config(dev=dev)
     # TODO: trigger configs from each workspace.
     # from <workspace>.config import config; config()
     # ...etc.
