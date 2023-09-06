ec00698eb50892fd98ceccce7358a09e9d5627a9
Majid alDosari <majid.aldosari@pnnl.gov>
2023-09-06 13:04:51 -0400

    [mapping] ontology filtering query function

--- mapping/src/mapping/util.py
+++ mapping/src/mapping/util.py
@@ -30,27 +30,56 @@ def get_data(src: BytesIO | ttl | Path | Callable[[], ttl ]  ) -> Triples:
         return _
     else:
         raise ValueError('dont know how to get data')
+    
+parts = {
+    's223': "standard223",
+    'rdfs': "rdf-schema",
+    'rdf': '22-rdf-syntax-ns',
+    'qudt': 'qudt',
+    'shacl': 'shacl',
+    }
+
+
+def make_regex_parts(parts):
+    for part in parts:
+        for sp in ('p', 'o'):
+            yield f"""regex(str(?{sp}), "{part}")"""
+
+
+mapped_pattern = "<<?s ?p ?o>> <http://meta> <<?ms <http://mmeta/query> ?mo>> ."
+ontology_pattern = '<<?s ?p ?o>> <http://meta> <<<http://mmeta/python#function> <http://mmeta/python#name> "get_ontology">> .'
 
 def filter_mapped():
     # so then these could just be used for the inferencing
-    _ = """
-    construct {?s ?p ?o }
-    where {
+    _ = f"""
+    construct {{?s ?p ?o }}
+    where {{
     ?s ?p ?o.
-    <<?s ?p ?o>> <http://meta> <<?ms <http://mmeta/query> ?mo>> .
-    #FILTER(
-    #    regex(str(?p), "data.ashrae.org/standard223" ) 
-    # || regex(str(?o), "data.ashrae.org/standard223" )
-    # || regex(str(?p), "www.w3.org/2000/01/rdf-schema" ) 
-    # || regex(str(?o), "www.w3.org/2000/01/rdf-schema" )
-    # || regex(str(?p), "www.w3.org/1999/02/22-rdf-syntax-ns" )
-    # || regex(str(?o), "www.w3.org/1999/02/22-rdf-syntax-ns" )
-    #)
-    }
+    # since all data went through a query to be mapped, this applies.
+    # but check if still applies when queries are used for something else.
+    {mapped_pattern}
+    }}
     """
+    # or can filter out
+    # <http://meta> <<<http://mmeta/python#function> <http://mmeta/python#name> "overlap">> .
     return _
 
 
 
+def filter_ontology():
+    # query to get data belonging to the ontology
+    _ = f"""
+    construct {{?s ?p ?o }}
+    where {{
+    ?s ?p ?o.
+    {{{ontology_pattern}}}
+    union
+    {{{mapped_pattern}}}
+    }}
+    """
+    # or could say NOT speckle.
+    # or filter FOR the 
+    # FILTER({ ' || '.join(make_regex_parts(parts.values())) } )
+    return _
 
 
