cf6140f31d76c72d3ecb186951b35b0ba96d53da
Majid alDosari <majid.aldosari@pnnl.gov>
2023-10-09 12:41:56 -0400

    [speckle] speckle non-dev not defined

--- speckle/src/speckle/config.py
+++ speckle/src/speckle/config.py
@@ -2,14 +2,17 @@
 def config(dev = True):
     from project import root
     _ = root / 'speckle' / 'secret.json'
-    if _.exists() == False: 
-        import json
-        input_data = input("enter your token: ")
-        json_object = {"speckl_token": input_data}
-        with open(_,"w") as f:
-            json.dump(json_object,f)
-    else: 
-        return None
+    if dev:
+        if not _.exists():
+            import json
+            input_data = input("enter your token: ")
+            json_object = {"speckl_token": input_data}
+            with open(_,"w") as f:
+                json.dump(json_object,f)
+        else: 
+            return _
+    else:
+        raise ValueError('speckle not configured')
 
 
 if __name__ == '__main__':
