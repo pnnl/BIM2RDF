6456b98d20286b2b0775750a854916ab7abb391e
Majid alDosari <majid.aldosari@pnnl.gov>
2023-10-10 17:26:56 -0400

    [mapping] add validation as a 'rule'

--- mapping/src/validation/engine.py
+++ mapping/src/validation/engine.py
@@ -7,10 +7,11 @@ from .shacl import shacl
 from mapping.conversions import og2rg, rg2og
 from ontologies import get
 
+
 from functools import lru_cache
 @lru_cache(1)
 def ontology():
-    _ = get('223p')
+    _ = get('s223')
     from rdflib import Graph
     g = Graph()
     g.parse(_)
@@ -20,21 +21,20 @@ def ontology():
 def _shacl_validation(db: OxiGraph):
     from mapping.engine import get_applicable
     _ = get_applicable(db._store)
-    #_ = OxiGraph()._store #gets stuck  here?!
     _ = og2rg(_)
     o = ontology()
-    #_ = shacl(_, shacl=None, mangling happens, so just taking it direcly from ontology
     _ = shacl(_, namespaces=o.namespaces(), shacl=o,
               ontology=None, advanced=False, )
     return _
 
-def shacl_validation(db: OxiGraph) -> Triples:
+
+def shacl_validation(db: OxiGraph,) -> Triples:
     _ = _shacl_validation(db)
     _ = _.validation.report
-    #for t in _: ss.add(t)
     _ = rg2og(_)
     _ = Triples((q.triple for q in _))
     return _
+
 
 import logging
 logger = logging.getLogger('validation')
@@ -42,18 +42,35 @@ logger = logging.getLogger('validation')
 class Engine(_Engine):
 
     def validate(self) -> Result:
+        from mapping.engine import PyRule
         logger.info('validating...')
-        _ = _shacl_validation(self.db)
-        conforms = _.validation.conforms
+        # one-time 'rule'
+        _ = PyRule(shacl_validation)
+        _ = _(self.db)
+        _.insert(self.db)
+        from mapping.utils.queries import namespaces
+        sh = {p:ns for p,ns in  namespaces()}
+        sh = sh['sh']
+        q = f"""
+        prefix sh: <{sh}>
+        select ?conforms where {{
+            ?_v sh:conforms ?conforms.
+        }}
+        """
+        _ = self._db._store.query(q)
+        _ = list(_)
+        assert(len(_) == 1)
+        conforms = _[0][0].value
+        if conforms == 'true':
+            conforms = True
+        else:
+            assert(conforms == 'false' )
+            conforms = False
         assert(isinstance(conforms, bool))
-        _ = _.validation.report
-        _ = rg2og(_)
-        _ = OxiGraph(_)
         return Result(_, conforms)
     
     def __call__(self) -> Result:
         _ = super().__call__()
         v = self.validate()
-        for q in v.db._store: _.db._store.add(q)
-        return _
+        return v
 
