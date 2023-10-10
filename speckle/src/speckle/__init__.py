fcdb02fb8652133acb2e40c219e60f616482a528
Majid alDosari <majid.aldosari@pnnl.gov>
2023-10-10 17:49:01 -0400

    [speckle] provision resolvable object uri

--- speckle/src/speckle/__init__.py
+++ speckle/src/speckle/__init__.py
@@ -1,2 +1,17 @@
-base_uri = lambda: 'http://speckle.systems/'
+base_uri = f"http://speckle.systems/"
 meta_uri = lambda: 'http://meta'
+
+
+def object_uri(stream_id, object_id):
+    return f"http://speckle.xyz/streams/{stream_id}/objects/{object_id}"
+
+
+def namespaces():
+    # spkl_arg 
+    from ontologies import namespace
+    from rdflib import URIRef
+    return (
+        namespace('spkl', URIRef(base_uri() )),
+        namespace('spkl.meta', URIRef(meta_uri() ) )
+    )
+
