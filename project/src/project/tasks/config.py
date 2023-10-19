6fb87a68333b1d06efb50c53bda974a12032cb5f
Majid alDosari <majid.aldosari@pnnl.gov>
2023-10-18 22:22:48 -0400

    [project] rem na

--- project/src/project/tasks/config.py
+++ project/src/project/tasks/config.py
@@ -4,11 +4,10 @@
 def config(dev = True):
     from project.tasks.git import config as gitconfig
     if dev: gitconfig(dev=dev)
-    from speckle.config import config; config(dev=dev)
-    from speckle.graphql import client;client(dev=dev)
-    from speckle.requests import get_session; get_session(dev=dev)
-    from project_azure.config import config; config(dev=dev)
-    from project_dvc.config import config; config(dev=dev)
+    from speckle.config import config;          config(dev=dev)
+    from project_azure.config import config;    config(dev=dev)
+    from project_dvc.config import config;      config(dev=dev)
+    from mapping.config import config;          config(dev=dev)
     #from graphdb.graphdb import config; config(dev=dev)
     # from <workspace>.config import config; config()
     # ...etc.
