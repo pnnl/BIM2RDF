8390f124fcac8cd0392607c0df0e667922491211
Majid alDosari <majid.aldosari@pnnl.gov>
2023-11-03 18:07:48 -0400

    [dev][speckle][mapping] geometry data handling lives in speckle

--- speckle/src/speckle/geometry.py
+++ speckle/src/speckle/geometry.py
@@ -643,14 +643,3 @@ class Comparison:
         return _
 
 
-# semantic stuff below
-
-from .engine import OxiGraph, Triples
-def overlap(db: OxiGraph) -> Triples:
-    #import heartrate; heartrate.trace(browser=True)
-    branch = 'architecture/rooms and lighting fixtures'
-    from itertools import product
-    combos = product({'Lighting Fixtures', }, {'Rooms', 'Spaces'}, {branch}  )
-    for o1s, o2s, b in combos:
-        for c in compare(db._store, o1s, o2s, b, b, analysis='fracInside'):
-            yield from c.triples()
