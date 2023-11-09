02abe2032a8f4767eabb3bcb32d68607205dd87d
Majid alDosari <majid.aldosari@pnnl.gov>
2023-11-09 12:59:16 -0500

    [mapping] graph_diff is a util

--- mapping/src/mapping/utils/graph.py
+++ mapping/src/mapping/utils/graph.py
@@ -1,5 +1,6 @@
 
-def graph_diff(data, generated):  # TODO: a util?
+def graph_diff(data, generated):
+    # this is just adding naming to the tuple elements
     from collections import namedtuple
     GraphDiff = namedtuple('GraphDiff', ['in_both', 'in_data', 'in_generated'])
     from rdflib.compare import graph_diff as _graph_diff
