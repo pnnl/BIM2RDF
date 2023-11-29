07f8a587cbd8f27da9f0ee4286343798c39c70bb
Shat Pratoomratana <shat.pratoomratana@pnnl.gov>
2023-11-29 11:01:15 -0800

    [mapping] Fixed issue where branch_ids could not be input as a set

--- mapping/src/mapping/speckle.py
+++ mapping/src/mapping/speckle.py
@@ -252,7 +252,7 @@ def engine(stream_id, *, branch_ids=None,
     if branch_ids is None:
         # figuring this is the default mode of working from now.
         data_rules = Rules([sg for sg in SpeckleGetter.multiple(stream_id) if sg.branch_id.split('/')[0].lower() in allowed_branches ])
-    elif isinstance(branch_ids, (list, tuple)):
+    elif isinstance(branch_ids, (list, tuple, set)):
         data_rules = Rules([sg for sg in SpeckleGetter.multiple(stream_id, branch_ids)])
     elif isinstance(branch_ids, str):
         data_rules = Rules([SpeckleGetter(stream_id, branch_id=branch_ids, object_id=object_id),])
--- mapping/tests/test_sp.py
+++ mapping/tests/test_sp.py
@@ -1,13 +1,32 @@
+#Test 1
+
 #Problem: mapping files that dont have a .mapping extention are being picked up. 
 #Test file to print which files get picked up.
 #Results: before files such as "mapping/s223/space-lightconnector.rq" were being picked up
 #After fixing the speckle.py file entries like the above without the .mapping are not picked up.
 
-import mapping.speckle as sp
-from mapping import mapping_dir
+import mapping.speckle as spek
+#from mapping import mapping_dir
 from icecream import ic
 
-maps_dir = mapping_dir / 's223'
+#maps_dir = mapping_dir / 's223'
 
-var = sp.maps(maps_dir=maps_dir)
+#var = spek.maps(maps_dir=maps_dir)
+#ic(var)
+    
+#~~~~~~~~~~~~~~~
+################
+#~~~~~~~~~~~~~~~
+#Test 2 
+#Problem: when doing the mapping branch_ids are not accepted when input as a set
+#Test that feeds branch_ids as a set to the engine func. 
+#Result: before fix if the branch_ids were given as a set a TypeError is raised
+#after fix if a set is fed in the mapping proceedes as normal
+
+
+branch_id = {'electrical/panels'} #Set
+stream_id = "Pritoni"
+
+var = spek.engine(stream_id, branch_ids=branch_id)
 ic(var)
+
