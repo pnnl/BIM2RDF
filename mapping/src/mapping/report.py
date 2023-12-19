dcb9a4410f5485c0696bf274ba94ff57b4348d02
Majid alDosari <majid.aldosari@pnnl.gov>
2023-12-19 12:38:59 -0500

    [mapping] fix filter

--- mapping/src/mapping/report.py
+++ mapping/src/mapping/report.py
@@ -9,16 +9,18 @@ class Report:
         _ = self.store.query(queries.shacl_report)
         _ = query_table(_)
         if filter:
+            mdq = set()
             for q in {
                 queries.rules.mapped,
-                queries.rules.rdfs_inferred,
-                queries.rules.shacl_inferred}:
+                #queries.rules.rdfs_inferred,
+                #queries.rules.shacl_inferred
+                }:
                 md = self.store.query(q)
                 md = tuple(md)
                 from itertools import chain
                 md = chain((t[0] for t in md), (t[2] for t in md))
                 md = (_.value for _ in md)
-                md = frozenset(md)
-                _ = _[_['focusNode'].isin(md)]
+                mdq.update(md)
+            _ = _[_['focusNode'].isin(mdq)]
         return _
 
