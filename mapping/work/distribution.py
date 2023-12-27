a52ae6c79c669c20636d4fe47818d34d6e0adce9
Majid alDosari <majid.aldosari@pnnl.gov>
2023-12-27 13:53:08 -0500

    [mapping] this is in distribution

--- mapping/work/distribution.py
+++ mapping/work/distribution.py
@@ -1,4 +1,12 @@
-# put distribution tasks here
+"""
+distribution tasks
+"""
+from pathlib import Path
+
+bdg2no = {
+    'Pritoni': 0,
+    'proto med-office': 1,
+}
 
 from graphdb.tasks import upload_graph
 
@@ -35,3 +43,63 @@ from graphdb.tasks import upload_graph
 #     else:
 #         _.dump(str(out), 'text/turtle')
 #     return out
+
+#namdspaces include "ours"
+#def replace_namespace
+
+class triplesqueryname(str):
+    def __init__(self, name) -> None:
+        super().__init__()
+        self.name = name
+        self.query
+    
+    @property
+    def query(self):
+        from mapping.utils.queries import queries
+        return getattr(queries.rules, self.name)
+
+
+def extract_triples(
+        ttl_or_dbdir: Path,
+        *queries: Path|triplesqueryname,
+        out: Path|None=Path('out.ttl')):
+    ttl_or_dir = Path(ttl_or_dbdir)
+    from pyoxigraph import Store
+    if ttl_or_dir.suffix == '.ttl':
+        from pyoxigraph import parse, Quad
+        s = Store()
+        s.bulk_extend(
+            Quad(*t)
+                for t in parse(ttl_or_dir, 'text/turtle'))
+    else:
+        assert(ttl_or_dir.is_dir())
+        s = Store(ttl_or_dir)
+    
+    from pyoxigraph import serialize
+    queriesargs = queries
+    queries = []
+    for q in queriesargs:
+        if not Path(q).exists(): # it's some name
+            queries.append(triplesqueryname(q).query)
+        else:
+            q = Path(q)
+            assert(q.suffix == '.rq')
+            q = q.read_text()
+            queries.append(q)
+
+    from mapping.utils.queries import select
+    if not out:
+        from io import BytesIO
+        out = BytesIO()
+    serialize(
+        select(s, queries),
+        out,
+        'text/turtle')
+    
+    
+    return out
+
+
+if __name__ == '__main__':
+    from fire import Fire
+    Fire({'extract_triples': extract_triples})
