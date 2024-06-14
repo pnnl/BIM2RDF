77610cbb7fab64af227eb4c4149f32993e52d99b
Majid alDosari <majid.aldosari@pnnl.gov>
2024-06-14 13:42:38 -0400

    [speckle] goes through all spkl sample

--- speckle/src/speckle/rec.py
+++ speckle/src/speckle/rec.py
@@ -1,18 +1,34 @@
 # using conversion using boltons.iterutils
 
 from json import load
-import select
-from boltons.iterutils import get_path, remap, research
-# #, # remap maybe dont need it. do it through research
+from boltons.iterutils import remap # get_path, research
 
 # use remap to (k, v) -> (k, ttl)
 # inplace string?
 
 
+from functools import cache
+@cache
+def bigjson():
+    _ = load(open('./data.json'))
+    return _
+
+class Object:
+    __slots__ = 'id', 'data'
+    def __init__(self, id, data={}) -> None:
+        self.id = id
+        self.data = data # self-refs :/
+    
+    def __repr__(self) -> str:
+        return f"@{self.id} {repr(self.data)}"
+
+    def update(self, items):
+        self.data.update(items)
+
 
 class MatrixList(list):
     def __str__(self, ):
-        return "matrix list"
+        return "encoded matrix list"
 
 terminals = {
     int, float, str,
@@ -22,25 +38,6 @@ terminals = {
     }
 terminals = tuple(terminals)
 
-
-from functools import cache
-@cache
-def json():
-    return load(open('./data.json'))
-
-
-class Object:
-    __slots__ = 'id', 'data'
-    def __init__(self, id, data={}) -> None:
-        self.id = id
-        self.data = data
-    
-    def __repr__(self) -> str:
-        return f"@{self.id} {repr(self.data)}"
-
-    def update(self, items):
-        self.data.update(items)
-        
 
 # need to identify.
 # need a 'context' thing...
@@ -53,7 +50,11 @@ def json():
 
 
 def json():
-    return {'id': '1', 'p': {'id': '2', 'pp': 3 }  }
+    return {'id': 1, 'p1':33, 'p': {'id': '2', 'pp': 3,  }  }
+
+
+def json():
+    return {'id':1, 'p':33, 'pl': {'id':2} }
 
 
 def pth2triples(p):
@@ -62,36 +63,37 @@ def pth2triples(p):
 
 
 def visit(p, k, v): # path, key, value
-    return True
     # keep for transformations?
-    if isinstance(v, Object):
-        return True
-    else:
-        return False
+    return True
+    # if isinstance(v, Object):
+    #     return True
+    # else:
+    #     return False
     # do the transform here
-    return p+(k,), v
-    if isinstance(v, terminals):
-        return k, str(v)
-    else:
-        assert(isinstance(v, list))
-        return k, v
-
+    #return p+(k,), v
+    # if isinstance(v, terminals):
+    #     return k, str(v)
+    # else:
+    #     return k, v
 
 def enter(p, k, v): # for creating 'parents'
-    if isinstance(v, dict):
+    if isinstance(v, dict) and 'id' in v:
         o = Object(v.pop('id'))
         return o, v.items()
+    elif isinstance(v, dict) and 'id' not in v:
+        return dict(), v.items()
     else:
-       assert(isinstance(v, terminals))
-       return v, False
-    
-def exit(p, k, v, new_obj, new_items):
+        isinstance(v, terminals)
+        return v, False
+
+def exit(p, k, v,
+         new_obj, new_items):
     new_obj.update(new_items)
     return new_obj
 
 
 def test():
-    _ = json()
+    _ = bigjson()
     _ = remap(_,
             visit=visit, 
             enter=enter,
