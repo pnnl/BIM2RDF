fb7799687eab17655c5c958c89be97838065e331
Majid alDosari <majid.aldosari@pnnl.gov>
2024-08-29 00:46:59 -0400

    [dvcdiff] works

--- dvcdiff/src/dvcdiff/files.py
+++ dvcdiff/src/dvcdiff/files.py
@@ -27,29 +27,39 @@ class Diff:
     def __init__(self, getter) -> None:
         self.g = self.getter = getter
     
-
-    def lines(self) -> list[str]:
+    def lines(self) -> '_.a|_.b':
         ext = self.g.pth.suffix[1:]
+        g = self.getter()
         if ext in {'txt', 'yaml', 'yml', 'json'}:
-            _ = self.g()
             _ = NS(    
-                a = _.a.read().decode().splitlines(),
-                b = _.b.read().decode().splitlines())
+                a = g.a.read().decode().splitlines(),
+                b = g.b.read().decode().splitlines())
             return _
+        else: # treat as binary
+            from hashlib import md5
+            return NS(
+                a = md5(g.a.read()).hexdigest(),
+                b = md5(g.b.read()).hexdigest(),
+            )
     
     def __call__(self, *p, **k):
         _ = self.lines()
-        from difflib import diff
+        from difflib import unified_diff as diff
         _ = diff(
-            _.a, _.b,
-            fromfile=self.g.a, tofile=self.g.b,
+            [l+'\n' for l in _.a],
+            [l+'\n' for l in _.b],
+            fromfile=str(self.g.pth),
+            tofile=  str(self.g.pth),
             n=3, lineterm='\n')
+        _ = ''.join(_)
         return _
 
 
 def diff(pth, *, reva=None, revb=None):
-    g = Get(a, b, reva=None, revb=None)
-    return Diff(g)()
+    g = Get(pth, reva=reva, revb=revb)
+    _ = Diff(g)
+    _ = _()
+    return _
 
 
 
