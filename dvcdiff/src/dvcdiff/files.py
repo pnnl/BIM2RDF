00f285be8b36bbb31b7d341cc0f8607d0f25c010
Majid alDosari <majid.aldosari@pnnl.gov>
2024-08-29 23:05:39 -0400

    [models][dvcdiff] generalize

--- dvcdiff/src/dvcdiff/files.py
+++ dvcdiff/src/dvcdiff/files.py
@@ -1,40 +1,55 @@
-from types import SimpleNamespace as NS
 
-uncommitted = 'uncommitted'
+current = 'current'
 class Get:
-    
+    from pathlib import Path    
     def __init__(self,
             pth, *,
-            reva=None, # a for before
-            revb=uncommitted # b for after
+            rev=current,
             ) -> None:
-        from pathlib import Path
-        self.pth = Path(pth)
-        self.reva = reva
-        self.revb = revb
+        self.pth = self.Path(pth)
+        self.rev = rev
     
-    from functools import cached_property
+    from functools import cached_property, cache
     @cached_property
     def fs(self):
-        if self.reva == uncommitted:
+        if self.rev == current:
             from fsspec.implementations.local import LocalFileSystem
-            a = LocalFileSystem()
+            return LocalFileSystem()
         else:
             from dvc.api import DVCFileSystem
-            a = DVCFileSystem(rev=self.reva)
-        if self.revb == uncommitted:
-            from fsspec.implementations.local import LocalFileSystem
-            b = LocalFileSystem()
-        else:
-            from dvc.api import DVCFileSystem
-            b = DVCFileSystem(rev=self.revb)
-        _ = NS(a=a, b=b)
-        return _
+            return DVCFileSystem(rev=self.rev)
     
-    def __call__(self, ):
-        class _:
-            a = self.fs.a.open((self.pth.as_posix())) 
-            b = self.fs.b.open((self.pth.as_posix()))
+    @cache
+    def read(self, mode='rb'):
+        from dvc.api import DVCFileSystem
+        if isinstance(self.fs, DVCFileSystem):
+            fp = self.Path(__file__).parent.parent.parent.parent
+            assert((fp / '.git').exists())
+            fp = self.pth.resolve().absolute().relative_to(fp)
+        else:
+            fp = self.pth
+        fp = fp.as_posix()
+        fp = str(fp)
+        return self.fs.open(fp, mode=mode).read()
+    
+    def __call__(self,):
+        return self.read()
+    
+    
+    def lines(self) -> '_.a|_.b':
+        ext = self.pth.suffix[1:]
+        if ext in txt_exts:
+            _ = self()
+            _ = _.decode()
+            _ = _.splitlines()
+        else: # treat as binary
+            _ = self()
+            _ = _.decode()
+            from hashlib import md5
+            _ = md5(_)
+            _ = _.hexdigest()
+            _ = [_]
+        _ = [l+'\n' for l in _]
         return _
 
 
@@ -42,50 +57,44 @@ txt_exts = {'txt', 'yaml', 'yml', 'json',
                    'rq', 'ttl' }
 
 class Diff:
-    def __init__(self, getter) -> None:
-        self.g = self.getter = getter
-    
-    def lines(self) -> '_.a|_.b':
-        ext = self.g.pth.suffix[1:]
-        g = self.getter()
-        if ext in txt_exts:
-            _ = NS(    
-                a = g.a.read().decode().splitlines(),
-                b = g.b.read().decode().splitlines())
-            return _
-        else: # treat as binary
-            from hashlib import md5
-            return NS(
-                a = md5(g.a.read()).hexdigest(),
-                b = md5(g.b.read()).hexdigest(),
-            )
+    def __init__(self,
+            gettera, getterb,
+            context=3) -> None:
+        self.ga = self.gettera = gettera
+        self.gb = self.getterb = getterb
+        self.context = context
     
     def __call__(self, *p, **k):
-        _ = self.lines()
         from difflib import unified_diff as diff
         _ = diff(
-            [l+'\n' for l in _.a],
-            [l+'\n' for l in _.b],
-            fromfile=str(self.g.pth),
-            tofile=  str(self.g.pth),
-            n=3, lineterm='\n')
+            self.ga.lines(),
+            self.gb.lines(),
+            fromfile=str(self.ga.pth),
+            tofile=  str(self.gb.pth),
+            n=self.context)
         _ = ''.join(_)
         return _
 
 
 from inspect import signature as sig
-def diff(pth, *,
-         reva=sig(Get.__init__).parameters['reva'].default,
-         revb=None):
+def diff(
+        path_a, *,
+        path_b=None,
+         reva=sig(Get.__init__).parameters['rev'].default,
+         revb=sig(Get.__init__).parameters['rev'].default,
+         context=sig(Diff.__init__).parameters['context'].default,
+         ):
     f"""
-    if reva or revb is '{uncommitted}'
+    if reva or revb is '{current}'
     it takes the content of the local (uncommitted) file
     """
-    g = Get(pth, reva=reva, revb=revb)
-    _ = Diff(g)
+    if path_b is None:
+        path_b = path_a
+    ga = Get(path_a, rev=reva)
+    gb = Get(path_b, rev=revb)
+    _ = Diff(ga, gb, context=context)
     _ = _()
     return _
-
 
 
 if __name__ == '__main__':
