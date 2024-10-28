7465395a9b631e2167b5090bfeb988e7f0f92936
Majid alDosari <majid.aldosari@pnnl.gov>
2024-10-28 16:20:20 -0400

    [topquadrant] check java as part of config

--- topquadrant/src/topquadrant/config.py
+++ topquadrant/src/topquadrant/config.py
@@ -1,3 +1,10 @@
 def config():
+    from .install import check_java
+    check_java()
     from .install import ShaclInstallation
     ShaclInstallation()
+
+
+if __name__ == '__main__':
+    from fire import Fire
+    Fire(config)
