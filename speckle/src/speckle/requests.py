c28770bb2fe34c84c155addaa380031a5242b139
Majid alDosari <majid.aldosari@pnnl.gov>
2025-01-10 17:31:42 -0500

    [speckle] request token from config

--- speckle/src/speckle/requests.py
+++ speckle/src/speckle/requests.py
@@ -2,15 +2,13 @@ from requests.auth import AuthBase
 
 class TokenAuth(AuthBase):
     def __init__(self,):
-        from .server import apikey
-        self.token = apikey()
+        from .config import token
+        self.token = token
         self.auth_scheme = 'Bearer'
         
     def __call__(self, request):
         request.headers['Authorization'] = f'{self.auth_scheme} {self.token}'
         return request
-
-
 
 def get_session( ):
     from requests import Session
