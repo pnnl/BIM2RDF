984e63a12e55ddb3508a3ec9991c59bcdef56fc2
Majid alDosari <majid.aldosari@pnnl.gov>
2025-01-10 17:46:14 -0500

    [speckle] data query working

--- speckle/src/speckle/data.py
+++ speckle/src/speckle/data.py
@@ -1,7 +1,16 @@
 
-def get_json(project_id, object_id):
+
+def json(*, project_id, object_id):
     from speckle.graphql import queries, query
-    _ = queries()
-    _ = _.objects(project_id, object_id)
+    _ = queries.objects(project_id, object_id)
     _ = query(_) # dict
     return _ 
+
+
+def rdf(*, project_id, object_id):
+    _ = json(project_id=project_id, object_id=object_id)
+    from .meta import prefixes
+    prefixes.meta, prefixes.concept
+    from json2rdf import j2r
+    _ = j2r(_) # TODO NOW the prefix stuff
+    return _
