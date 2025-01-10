198a8dbfc74af91870bc1e1daaf5347f03a664d6
Majid alDosari <majid.aldosari@pnnl.gov>
2025-01-10 17:17:56 -0500

    [speckle] spkle meta

--- speckle/src/speckle/__init__.py
+++ speckle/src/speckle/__init__.py
@@ -1,15 +1,3 @@
 
-def object_uri(project_id, object_id=''):
-    from .config import server
-    return f"http://{server}/projects/{project_id}/objects/{object_id}"
 
-
-def namespaces():
-    # spkl_arg 
-    from ontologies import namespace
-    from rdflib import URIRef
-    return (
-        namespace('spkl', URIRef(base_uri() )),
-        namespace('spkl.meta', URIRef(meta_uri() ) )
-    )
 
---
+++ speckle/src/speckle/meta.py
@@ -0,0 +1,13 @@
+
+class object:
+    @staticmethod
+    def uri(project_id, object_id=''):
+        from .config import server
+        return f"http://{server}/projects/{project_id}/objects/{object_id}"
+
+
+class prefixes:
+    from bim2rdf.rdf import NameSpace
+    concept =   NameSpace('spkl',       "urn:speckle:concept")
+    meta =      NameSpace('spkl.meta',  "urn:speckle:meta")
+
