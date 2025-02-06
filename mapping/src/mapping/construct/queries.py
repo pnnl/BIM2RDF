998d13b036555fb147cae70685f01413d20d4bca
Majid alDosari <majid.aldosari@pnnl.gov>
2025-02-05 21:33:57 -0500

    [mapping] more flexibl query construction

--- mapping/src/mapping/construct/queries.py
+++ mapping/src/mapping/construct/queries.py
@@ -1,47 +1,78 @@
-
 from pathlib import Path
+from typing import Self
 class Query:
-    def __init__(self, p: Path, substitutions={}):
+    class defaults:
+        dir = Path(__file__).parent
+        from speckle.meta import prefixes as spkl_prefixes
+        substitutions = {
+            'prefix.spkl':      spkl_prefixes.concept.uri,
+            'prefix.spkl.meta': spkl_prefixes.meta.uri,
+            'model.arch.rooms&lights.name': 'architecture/rooms and lighting fixtures',
+        }
+
+    def __init__(self, _s: str, *, substitutions=defaults.substitutions):
+        self._s = _s
+        self.substitutions = substitutions.copy()
+    
+    @classmethod
+    def from_path(cls, p: Path, *, substitutions=defaults.substitutions) -> Self:
         p = Path(p)
         assert(p.exists())
-        self.path = p
-        self.substitutions = substitutions.copy()
+        return cls(p.read_text(), substitutions=substitutions)
 
     from functools import cached_property
     @cached_property
     def string(self):
-        _ = self.path.read_text()
+        _ = self._s
         from bim2rdf.utils.substitution import String
         _ = String(_)
         _ = _.substitue(self.substitutions)
         return _
     def __str__(self): return self.string
+
+    from typing import Iterable
+    @classmethod
+    def s(cls,
+          source: Iterable = tuple(defaults.dir.glob('**/*.rq')),
+          *, substitutions=defaults.substitutions) -> Iterable[Self]:
+        for src in source:
+            if isinstance(src, str):
+                if Path(src).exists(): src = Path(src)
+                else: src = None
+            else:
+                assert(isinstance(src, Path))
+                assert(src.exists())
+            if src:
+                assert(isinstance(src, Path))
+                _ = cls.from_path(src, substitutions=substitutions)
+                _.source = src
+                yield _
+            else:
+                assert(src is None)
+                yield cls(_, substitutions)
     
-
-from speckle.meta import prefixes as spkl_prefixes
-substitusions = {
-    'x': 'xv',
-    'prefix.spkl':      spkl_prefixes.concept.uri,
-    'prefix.spkl.meta': spkl_prefixes.meta.uri,
-    'model.arch.rooms&lights.name': 'architecture/rooms and lighting fixtures',
-}
-
-_ = Path(__file__).parent
-_ = _.glob('**/*.rq')
-_ = (Query(_, substitutions=substitusions) for _ in _)
-shipped = tuple(_)
-del _
-
+default = tuple(Query.s())
 
 if __name__ == '__main__':
-    def _(out=Path('construct')):
-        out = Path(out)
-        for q in shipped:
-            p = q.path.relative_to(Path(__file__).parent)
-            p: Path = out / p
+    from typing import Literal
+    def _(*, i=Path(Query.defaults.dir),
+          substitutions: Path|Literal['default']='default',
+          o=Path('construct')):
+        i = Path(i)
+        if substitutions == 'default':
+            substitutions = Query.defaults.substitutions
+        else:
+            substitutions = Path(substitutions)
+            assert(substitutions.exists())
+            from yaml import safe_load
+            substitutions = safe_load(open(substitutions))
+        o = Path(o)
+        for q in Query.s(i.glob('**/*.rq'), substitutions=substitutions):
+            p = q.source.relative_to(i)
+            p: Path = o / p
             p.parent.mkdir(parents=True, exist_ok=True)
             open(p, 'w').write(q.string)
-        return out
+        return o
 
     from fire import Fire
     Fire(_)
