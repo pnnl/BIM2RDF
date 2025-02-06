f00d280c04a850d89ba2294d19faf52ccab0bded
Majid alDosari <majid.aldosari@pnnl.gov>
2025-02-06 10:36:47 -0500

    [project] not used

--- project/src/project/tasks/config.py
+++ project/src/project/tasks/config.py
@@ -4,16 +4,6 @@
 def config(dev = True):
     from project.tasks.git import config as gitconfig
     if dev: gitconfig(dev=dev)
-    from speckle.config import config;          config(dev=dev)
-    from project_azure.config import config;    config(dev=dev)
-    from project_dvc.config import config;      config(dev=dev)
-    from topquadrant.config import config;      config()
-    from models.config import config;           config(dev=dev)
-    #from graphdb.graphdb import config; config(dev=dev)
-    # from <workspace>.config import config; config()
-    # ...etc.
-    # order might matter
-    ...
 
 
 if __name__ == '__main__':
