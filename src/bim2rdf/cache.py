3d76f5aa627e63851e82af3687d0087c966fb966
Majid alDosari <majid.aldosari@pnnl.gov>
2025-02-18 16:11:29 -0500

    [src] gitignore cache dir contents

--- src/bim2rdf/cache.py
+++ src/bim2rdf/cache.py
@@ -3,6 +3,8 @@ try:
     from pathlib import Path
     dir = Path('.') / 'cache'
     dir.mkdir(exist_ok=True)
+    (dir / '.gitignore').touch()
+    (dir / '.gitignore').write_text('*')
     def cache(f, *p, dir=dir, **k):
         from cachier import cachier
         return cachier(*p, cache_dir=dir, **k)(f)
