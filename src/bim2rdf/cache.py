378a51ad105b75be1db2ffc0c4fb1b57dbf59291
Majid alDosari <majid.aldosari@pnnl.gov>
2025-04-14 13:18:28 -0400

    [src] fix: check if installed first for non-dev scenario

--- src/bim2rdf/cache.py
+++ src/bim2rdf/cache.py
@@ -1,14 +1,14 @@
 """persistent caching"""
 try:
+    from cachier import cachier
     from pathlib import Path
     dir = Path('.') / 'cache'
     dir.mkdir(exist_ok=True)
     (dir / '.gitignore').touch()
     (dir / '.gitignore').write_text('*')
     def cache(f, *p, dir=dir, **k):
-        from cachier import cachier
         return cachier(*p, cache_dir=dir, **k)(f)
-except:
+except ModuleNotFoundError:
     dir = None
     cache = lambda f, *p, **k: f
 
