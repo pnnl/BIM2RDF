fc594f069f5a7843d89469e81bbdc7af45ad5f73
Majid alDosari <majid.aldosari@pnnl.gov>
2025-04-15 10:31:54 -0400

    [src] centralize fix

--- src/bim2rdf/cli.py
+++ src/bim2rdf/cli.py
@@ -48,34 +48,6 @@ def attdoc(f):
     f.__doc__ = _
     return f
 run = attdoc(run)
-### patch for dev env until new release
-# https://github.com/google/python-fire/issues/589
-import fire.inspectutils as fi
-def _(component):
-  try:
-    from IPython.core import oinspect  # pylint: disable=import-outside-toplevel,g-import-not-at-top
-    try:
-      inspector = oinspect.Inspector(theme_name="Neutral")
-    except TypeError:  # Only recent versions of IPython support theme_name.
-      inspector = oinspect.Inspector()
-    info = inspector.info(component)
-    # IPython's oinspect.Inspector.info may return '<no docstring>'
-    if info['docstring'] == '<no docstring>':
-      info['docstring'] = None
-  except ImportError:
-    info = fi._InfoBackup(component)
-  try:
-    import inspect
-    unused_code, lineindex = inspect.findsource(component)
-    info['line'] = lineindex + 1
-  except (TypeError, OSError):
-    info['line'] = None
-  if 'docstring' in info:
-    from fire import docstrings
-    info['docstring_info'] = docstrings.parse(info['docstring'])
-  return info
-fi.Info = _
-###
 from fire import Fire
 run = Fire(run)
 exit(0)
