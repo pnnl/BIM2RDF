6bf24f153058f730ac1acd0e73a89c076356df2c
Majid alDosari <majid.aldosari@pnnl.gov>
2025-10-21 13:20:46 -0400

    line is here

--- core/src/bim2rdf/core/__init__.py
+++ core/src/bim2rdf/core/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '2025.10.1845'
+__version__ = '2025.10.1846'
 # reset to 0 if problem
 from .engine import *
--- spklauto/src/bim2rdf/spklauto/main.py
+++ spklauto/src/bim2rdf/spklauto/main.py
@@ -194,7 +194,6 @@ if __name__ == "__main__":
     # NOTE: always pass in the automate function by its reference; do not invoke it!
     # Pass in the function reference with the inputs schema to the executor.
     #execute_automate_function(automate_function, FunctionInputs)
-    execute_automate_function(automate_function_without_inputs, )
 
     # If the function has no arguments, the executor can handle it like so
-    # execute_automate_function(automate_function_without_inputs)
+    execute_automate_function(automate_function_without_inputs)
